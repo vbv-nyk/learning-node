e4f859d978a0bd2267383badcf598a64649f7a1d
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-01-26 17:01:50 +0530

    Add a seperate file for routes and used it as a module

--- creating-server/app.js
+++ creating-server/app.js
@@ -1,34 +1,6 @@
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<h1>Fill in your details</h1>");
-    res.write("<form method='POST' action='/message'>");
-    res.write("<input type='text' name='message'>");
-    res.write("<button type='submit'>Submit</button>");
-    res.write("</form>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const bufferData = [];
-    req.on("data", (chunk) => {
-      bufferData.push(chunk);
-    });
-    return req.on("end", () => {
-      const data = Buffer.concat(bufferData).toString();
-      const message = data.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("location", "/");
-        return res.end();
-      });
-    });
-  }
-  res.write("Thank you for submitting your feedback!");
-  res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
