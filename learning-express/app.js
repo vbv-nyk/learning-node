976c77bdb33e35c601a9dffa81c6a337592ca57b
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-01-26 19:16:47 +0530

    Funneled through middlewares

--- learning-express/app.js
+++ learning-express/app.js
@@ -2,14 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("You're in the first middleware");
+app.use("/", (req, res, next) => {
+  console.log("This always runs");
   next();
 });
 
-app.use((req, res, next) => {
-  console.log("You're in the second middleware");
-  res.send("<h1>Congrats on making it through the middlewares</h1>");
+app.use("/about-me", (req, res, next) => {
+  console.log("Express is handling the path /about-me");
+  res.send(`<h1>About me</h1>`);
+});
+
+app.use("/", (req, res, next) => {
+  res.send(
+    "<h1>After checking all above paths, you come here. You did have a long way to go ;)</h1>"
+  );
 });
 
 app.listen(3000);
