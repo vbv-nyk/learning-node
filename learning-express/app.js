6f8f7721e23c6d5c8850358403e6b70815a75f4f
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-01-26 19:09:50 +0530

    Used app(instance of Express) to learn middlewares

--- learning-express/app.js
+++ learning-express/app.js
@@ -1,5 +1,15 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
+
+app.use((req, res, next) => {
+  console.log("You're in the first middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("You're in the second middleware");
+  res.send("<h1>Congrats on making it through the middlewares</h1>");
+});
+
+app.listen(3000);
