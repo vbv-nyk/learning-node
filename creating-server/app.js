26ce24888b5f1b2f86692d9eacdd54cd446efa97
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-01-26 14:23:51 +0530

    Accesing some fields from request

--- creating-server/app.js
+++ creating-server/app.js
@@ -1,8 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  process.exit();
+  console.log(req.url, req.method, req.headers);
 });
 
 server.listen(3000);
