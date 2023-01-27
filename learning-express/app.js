3b226abd3135a82de11498066f94d768de07f933
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-01-27 10:00:35 +0530

    Using routes from app.js

--- learning-express/app.js
+++ learning-express/app.js
@@ -5,7 +5,7 @@ const adminPage = require("./routes/admin");
 const shopPage = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(adminPage);
+app.use("/admin", adminPage);
 app.use(shopPage);
 app.use("/", (req, res, next) => {
   res.status(404).send("<h3>Page not found</h.status(404)3>");
--- learning-express/routes/admin.js
+++ learning-express/routes/admin.js
@@ -4,11 +4,11 @@ const router = express.Router();
 
 router.post("/add-item", (req, res, next) => {
   console.log(req.body);
-  res.redirect("/add-item");
+  res.redirect("/admin/add-item");
 });
 
 router.get("/add-item", (req, res, next) => {
-  res.send(`<form method="POST" action="/add-item">
+  res.send(`<form method="POST" action="/admin/add-item">
           <input type="text" name="message"/>
           <button type="submit">Button</button>
       </form>`);
