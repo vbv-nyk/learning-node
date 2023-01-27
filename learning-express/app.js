376015889e99c0fcf4f742eb0d79c1de7aa508df
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-01-27 09:21:04 +0530

    Using routers to go to websites

--- learning-express/app.js
+++ learning-express/app.js
@@ -1,22 +1,11 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const adminPage = require("./routes/admin");
+const shopPage = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use("/thank-you", (req, res, next) => {
-  res.send("<h1>Thank you for submitting your feeback</h1>");
-});
-app.post("/items", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/thank-you");
-});
-
-app.use("/", (req, res, next) => {
-  res.send(`<form method="POST" action="/items">
-        <input type="text" name="message"/>
-        <button type="submit">Button</button>
-    </form>`);
-});
+app.use(adminPage);
+app.use(shopPage);
 
 app.listen(3000);
---
+++ learning-express/routes/admin.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.use("/thank-you", (req, res, next) => {
+  res.send("<h1>Thank you for submitting your feeback</h1>");
+});
+
+router.post("/items", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/thank-you");
+});
+
+router.get("/add-item", (req, res, next) => {
+  res.send(`<form method="POST" action="/items">
+          <input type="text" name="message"/>
+          <button type="submit">Button</button>
+      </form>`);
+});
+
+module.exports = router;
